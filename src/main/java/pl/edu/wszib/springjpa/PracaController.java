6044a1a5683bcee01c400693e953fce032a8c466
arkawa <arturkawa111@gmail.com>
2023-03-19 12:49:52 +0100

    database java

---
+++ src/main/java/pl/edu/wszib/springjpa/PracaController.java
@@ -0,0 +1,48 @@
+package pl.edu.wszib.springjpa;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/praca")
+public class PracaController {
+
+    @Autowired
+    private PracaRepository repository;
+
+    @GetMapping
+    public List<Praca> list(@RequestParam (required = false) String stanowisko) {
+        if (stanowisko==null){
+            return repository.findAll();
+        }
+        return repository.findAllByStanowisko(stanowisko);
+    }
+
+    @GetMapping("/{id}")
+    public Praca get(@PathVariable Integer id) {
+        return repository.findById(id).get();
+    }
+
+    @PostMapping
+    public Praca create(@RequestBody Praca praca) {
+        praca.setId(null);
+        return repository.save(praca);
+    }
+
+    @PutMapping
+    public Praca update(@RequestBody Praca praca) {
+        Praca exist = get(praca.getId());
+        exist.setStanowisko(praca.getStanowisko());
+        exist.setFirma(praca.getFirma());
+
+        return repository.save(exist);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        repository.deleteById(id);
+    }
+}
